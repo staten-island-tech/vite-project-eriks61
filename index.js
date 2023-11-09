99ea7e2fd0bd196acbccc85e078ee42fb7b76aa5
eriks61 <eriks61@nycstudents.net>
2023-11-09 09:21:21 -0500

    fbvc

--- index.js
+++ index.js
@@ -1,1 +1,123 @@
 import "./style.css";
+
+const menuItem = [
+  {
+    FoodName: "Scrambled Eggs",
+    Price: 12,
+    GlutenFree: true,
+    Meal: "Breakfast",
+    Cheap: true,
+  },
+  {
+    FoodName: "Ham Sandwich",
+    Price: 13,
+    GlutenFree: true,
+    Meal: "Lunch",
+    Cheap: true,
+  },
+  {
+    FoodName: "Ribeye Steak",
+    Price: 40,
+    GlutenFree: false,
+    Meal: "Dinner",
+    Cheap: false,
+  },
+  {
+    FoodName: "Burger",
+    Price: 22,
+    GlutenFree: false,
+    Meal: "Lunch",
+    Cheap: false,
+  },
+  {
+    FoodName: "Chicken Sandwich",
+    Price: 17,
+    GlutenFree: true,
+    Meal: "Lunch",
+    Cheap: true,
+  },
+  {
+    FoodName: "Salmon",
+    Price: 23,
+    GlutenFree: true,
+    Meal: "Dinner",
+    Cheap: false,
+  },
+  {
+    FoodName: "Lamb Chops",
+    Price: 30,
+    GlutenFree: false,
+    Meal: "Dinner",
+    Cheap: false,
+  },
+  {
+    FoodName: "Lamb Gyro",
+    Price: 17,
+    GlutenFree: false,
+    Meal: "Lunch",
+    Cheap: true,
+  },
+  {
+    FoodName: "Pancakes",
+    Price: 14,
+    GlutenFree: false,
+    Meal: "Breakfast",
+    Cheap: true,
+  },
+  {
+    FoodName: "French Toast",
+    Price: 21,
+    GlutenFree: false,
+    Meal: "Breakfast",
+    Cheap: false,
+  },
+  {
+    FoodName: "English Breakfast",
+    Price: 30,
+    GlutenFree: false,
+    Meal: "Breakfast",
+    Cheap: false,
+  },
+  {
+    FoodName: "Sushi",
+    Price: 45,
+    GlutenFree: true,
+    Meal: "Lunch",
+    Cheap: false,
+  },
+  {
+    FoodName: "House Salad",
+    Price: 15,
+    GlutenFree: true,
+    Meal: "Lunch",
+    Cheap: true,
+  },
+  {
+    FoodName: "Seafood Platter",
+    Price: 55,
+    GlutenFree: true,
+    Meal: "Dinner",
+    Cheap: false,
+  },
+  {
+    FoodName: "Beef Wellington",
+    Price: 44,
+    GlutenFree: false,
+    Meal: "Dinner",
+    Cheap: false,
+  },
+  {
+    FoodName: "Chicken and Rice",
+    Price: 22, 
+    GlutenFree: true,
+    Meal: "Dinner",
+    Cheap: false
+  },
+  {
+    FoodName: "Tacos",
+    Price: 17, 
+    GlutenFree: false,
+    Meal: "Lunch",
+    Cheap: true
+  },
+];
