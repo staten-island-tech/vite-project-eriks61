7b6d792a85708e78dc0c93d60d6e9bfb27d7f8c4
eriks61 <eriks61@nycstudents.net>
2023-11-13 09:22:45 -0500

    dfg

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,3 +1,5 @@
 import { menuItems } from "./arr.js";
 
 const cheap = menuItems.filter((menuItem)=> menuItem.Cheap = true).forEach((menuItem) => console.log(menuItem.FoodName));
+
+const Glutenfree = menuItems.filter((menuItem)=> menuItem.GlutenFree = true).forEach((menuItem) => console.log(menuItem.FoodName));
--- vite-project/js/index.js
+++ vite-project/js/index.js
@@ -2,3 +2,4 @@ import "../css/style.css";
 
 import { menuItem } from "./arr";
 console.log(menuItem)
+
